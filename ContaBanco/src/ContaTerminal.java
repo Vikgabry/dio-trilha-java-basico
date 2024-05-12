5c97bcebd3d57fd8770ecf8049103c7bbc9f82d7
ViktorG <gabryoliver22@gmail.com>
2024-05-12 18:34:41 -0300

    Feito 100% do Codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+import java.util.Locale;;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner ler = new Scanner(System.in).useLocale(Locale.US);
+       
+
+       int numero;
+       String agencia,nomeCliente,sobrenomeCliente;
+       double saldo;
+
+       //Pede o numero da conta para o cliente.
+       System.out.println("Digite o numero da conta:  ");
+       numero = ler.nextInt();
+
+       //Pede o numero da agencia para o cliente.
+       System.out.println("DIgite o numero da agencia:");
+       agencia = ler.next();
+    
+       //Pede o nome do cliente para o cliente.
+       System.out.println("Digite seu nome:");
+       nomeCliente = ler.next();
+
+       //Pede o sobrenome do cliente para o cliente.
+       System.out.println("Digite seu sobrenome");
+       sobrenomeCliente = ler.next();
+
+       //Pede o saldo atual do cliente para o cliente.
+       System.out.println("Digite o saldo:");
+       saldo = ler.nextDouble();
+
+       //Imprime os dados do cliente.
+       System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta "+ numero + " e seu saldo " + saldo +  " já está disponível para saque.");
+
+       ler.close();
     }
 }
